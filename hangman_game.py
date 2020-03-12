082737f246f3d545c64497744b3c8e9dd78477a7
ahammadsk-000 <61971118+ahammadsk-000@users.noreply.github.com>
2020-03-12 12:52:39 +0530

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -1,8 +1,9 @@
-# original Hangman with letter bug = selecting the same correct letter over and over will win the game
+# original Hangman with letter bug = selecting the same correct letter
+#over and over will win the game
 
 import random
 
-answerlist = ["word","look","hello","goodbye"]
+answerlist = ["goose","swan","penguin","ostrich","eagle"]
 
 random.shuffle(answerlist)
 
@@ -11,13 +12,17 @@ answer = list(answerlist[0])
 #print(answer)
 # empty list called display
 display = []
-# this will contain the used letters
+# used  will contain all the  letters in the answer
+# these will be taken away as the user guesses each one 
 used= [] 
 
-# adds the variable answer to display
+# extend will split the word into individual letter
+# in the list eg yes becomes y,e,s
 display.extend(answer)
-# adds what is in display ie the answer to used
-#so we can take letters out
+
+# this copies the list of letters from display into 'used'
+# this will enable us to work out if a letter has been used or not
+
 used.extend(display)
 
 #print(display)
@@ -35,30 +40,40 @@ print()
 # counter stops the game once all letters have been guessed
 count =0
 
+incorrect = 5
+
 # keep asking the user until all letters guessed
-while count < len(answer):
+while count < len(answer) and incorrect >0:
+    print("clue is the word based on birds")
     guess = input("please guess a letter: ")
+    
     guess = guess.lower()
     print(count)
     
     # iterates through the letter in answer
     for i in range(len(answer)):
-        # if the guessed letter maches a letter 
+        # if the guessed letter matches a letter 
         # in the answer
-        if answer[i] == guess:
+        if answer[i] == guess and guess in used:
             # replace the index of the guess with
             # The actual letter they guessed
             display[i]= guess
-            counter = count + 1
+            count = count + 1
             
             # print(used)
             used.remove(guess)
     if guess not in display:
-        print("Sorry, wrong guess")
+        incorrect = incorrect - 1
+        print("Sorry, wrong guess. you have",incorrect,"chance 1")
+
+    print("you have guessed: ",count,"correct letter.")
+    print("you have",incorrect,"chances left")
         
-    print("You have guessed: ",count,"correct letters.")
     # print out the new string with the guessed letters in
     print(' '.join(display))
     print()
-    
-print("Well done, you guess the word")
+if count == len(answer):
+# once count is greater than length of word then :
+    print("Well done, you guess the word")
+else:
+    print("Unfortunately you ran out of guess")
